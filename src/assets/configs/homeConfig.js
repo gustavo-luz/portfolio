5271bf2c8340c93f941f87924fb09eb6a5a6197f
gustavo-luz <gustavo.luz.unb@gmail.com>
2022-12-10 01:59:19 -0300

    replaces colours, basic info and part of home

--- src/assets/configs/homeConfig.js
+++ src/assets/configs/homeConfig.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {BsClipboardData} from "react-icons/bs";
 import {DiCodeigniter} from "react-icons/di";
+import {SiSamsung} from "react-icons/si";
+import {FcElectronics} from "react-icons/fc"
 import {GiCommercialAirplane} from "react-icons/gi";
 import {FaMobileAlt} from "react-icons/fa";
 
@@ -8,42 +10,39 @@ const homeConfig = {
 
     greeting:
         <h1 className="heading">
-            Hi! I'm <strong className="main-name"> Rodrigo Arenas</strong>
+            Hi! I'm <strong className="main-name"> Gustavo Luz</strong>
         </h1>,
     titles: [
         "Data Scientist",
-        "Machine Learning Specialist",
-        "Open Source Contributor",
-        "Backend Developer",
-        "Colombian"
+        "Artificial Intelligence and IoT Researcher",
+        "Production Engineer",
+        "Brazilian"
     ],
     about: {
-        start: "I've been working for over six years on data science projects. " +
-            "I'm excited by learning new things, contributing to the data science community," +
-            " and spreading the word of data!",
-        exit: "I'm fluent at Python, SQL databases, BI tools, and more, " +
-            "with a deep interest in machine learning."
+        start: "I've been working for over 3 years on data science and internet of things projects. " +
+            "I'm excited by applying machine learning and deep learning techniques at data collected by sensors",
+        exit: "I have experience with Python, R, Matlab, C++, BI tools, and more skills showed at this portfolio "
     },
     workTimeline: [
         {
             id: "work-3",
-            title: "Sr Data Scientist",
-            company: "Globant",
-            description: "Build AutoML models and make them available to non-technical " +
-                "users from a graphical interface.",
+            title: "Jr Data Scientist",
+            company: "Samsung R&D Institute Brazil",
+            description: "Sensor data analysis and prediction on wearables." +
+                "Works with data pre-processing, labeling, feature engineering, model choice and evaluation.",
             date: "2021-present",
-            icon: <DiCodeigniter/>,
-            tags: ["ml", "automl", "python", "docker", "redshift", "superset"]
+            icon: <SiSamsung/>,
+            tags: ["python", "matlab", "c", "machine learning", "deep learning"]
         },
         {
             id: "work-2",
-            title: "BI Manager",
-            company: "Rappi",
-            description: "Leading a team of analysts and data scientists to execute different projects, " +
-                "including the construction of ETLs, data lakes, data warehouses, and machine learning models.",
+            title: "Lab Researcher",
+            company: "UIoT",
+            description: "Development of two iot systems: one for smart cities and one envolving computer vision. " +
+                "3 international papers published and 1 patent submitted",
             date: "2019-2021",
-            icon: <FaMobileAlt/>,
-            tags: ["ml", "airflow", "python", "docker", "snowflake", "postgres", "azure"]
+            icon: <FcElectronics/>,
+            tags: ["python", "c++", "docker", "flask", "postgres", "opencv"]
         },
         {
             id: "work-1",
